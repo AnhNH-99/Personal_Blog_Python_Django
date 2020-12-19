f894797dc3cdd2c62613ae16aa28792c7be72437
Hoàng Anh Nguyễn <hoanganhkfe99@gmail.com>
2020-12-19 17:46:33 +0700

    Update code

--- Personal_Blog/settings.py
+++ Personal_Blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '(755@ds9#)38#jalxf==_md6l53)9kmyge&vo$^-4*o^@xtzvi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jack-blogger.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
